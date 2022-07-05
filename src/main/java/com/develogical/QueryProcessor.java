a2bbf722ab5e27c9d698e363ee6af5582144cb86
HaasTimon <timon@familie-haas.net>
2022-07-05 10:40:52 +0200

    Added QueryProcess

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,10 +8,13 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        if (query.toLowerCase().contains("tum")) {
+        else if (query.toLowerCase().contains("tum")) {
             return "The TUM is an excellent university in Munich, Bavaria" +
                     "\n Established in 1868, its one of the largest Universitys in Germany " +
                     "\n Way better than the LMU ;D";
+        }
+        else if (query.toLowerCase().contains("name")) {
+            return "Timon";
         }
         return "";
     }
